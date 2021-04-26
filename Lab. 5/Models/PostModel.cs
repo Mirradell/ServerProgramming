316343613bde3545be159195b34abd8054462e2f
Mirradell <olya.shtejn@yandex.ru>
2021-04-26 18:29:57 +0300

    init 5 lab

---
+++ Lab. 5/Models/PostModel.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lab._5.Models
+{
+    //Записи (ид, дата, текст, автор, изображение, лайки).
+    public class Post
+    {
+        public string ID { get; set; }
+        public DateTime createdDate { get; set; }
+        public string Text { get; set; }
+        public string ImageURL { get; set; }
+        public int LikeCount { get; set; }
+        public int UserId { get; set; }
+        public User Author {get; set;}
+    }
+
+    public class PostModel
+    {
+        public List<Post> items;
+        private User users = new Lab._5.Models.UserModel().item;
+
+        public PostModel()
+        {
+            items = JsonConvert.DeserializeObject<List<Post>>(File.ReadAllText("Data/posts.json"));
+
+            foreach (var post in items)
+            {
+                post.Author = users;
+            }
+        }
+    }
+}
---
+++ Lab. 5/Models/UserModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace Lab._5.Models
+{
+    //Пользователи (ид, имя, ник, подписки, подписчики, лайки);
+    public class User
+    {
+        public string ID { get; set; }
+        public string Name { get; set; }
+        public string UserName { get; set; }
+
+        [Display(Name = "UsersImage")]
+        public string ImageURL { get; set; }
+        public string About { get; set; }
+        public List<User> Followers { get; set; }
+        public List<User> Following { get; set; }
+    }
+    public class UserModel
+    {
+        public User item;
+        public UserModel()
+        {
+            item = JsonConvert.DeserializeObject<User>(File.ReadAllText("Data/users.json"));
+        }
+    }
+}
